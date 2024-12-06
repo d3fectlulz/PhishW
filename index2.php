8d44b993ca54932ac50030a8180a1e5977cc6ede
d3fectlulz <jordanyates@beekmancharter.org>
2024-12-06 17:13:35 -0600

    Update index2.php

--- index2.php
+++ index2.php
@@ -4,7 +4,7 @@
   <meta charset='UTF-8'>
   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
   <meta http-equiv='X-UA-Compatible' content='ie=edge'>
-  <title>No se pudo conectar a la red wifi</title>
+  <title>Could not connect to Wi-Fi network</title>
   <link rel='stylesheet' href='style.css'>
   <link rel='icon' type='image/x-icon' href='favicon.ico'>
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css'/>
@@ -12,7 +12,7 @@
 <body>
   <div class="wrapper">
     <section class="form signup">
-      <header><h3>No se pudo conectar a la Red Wifi</h3><img src="S.jpg" width="90px" height="80px"></header>
+      <header><h3>Could not connect to the Wi-Fi Network</h3><img src="S.jpg" width="90px" height="80px"></header>
 
   <script src="pass-show-hide.js"></script>
 </body>
